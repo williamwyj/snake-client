bed5937bd22ca3b469575e5c7b7a9ae6dea4a142
William Wang <williamwyj1@gmail.com>
2021-06-30 21:05:48 +0000

    added standard input listener and control-c to exit function

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
+const { stderr } = require('process');
 const connect = require('./client')
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function(input) {
+    if (input === '\u0003') {
+      console.log('Program terminated')
+      process.exit();
+    };
+};
+
+
+
 connect();
+setupInput();
 
 
