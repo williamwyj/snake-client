bb3ff47e14197b70f1781327124f941ffbe73022
William Wang <williamwyj1@gmail.com>
2021-06-30 22:46:23 +0000

    updated to send message with key word

--- input.js
+++ input.js
@@ -1,3 +1,9 @@
+const { stdin } = require("process");
+const readline = require('readline')
+const rl = readline.createInterface({
+  input : process.stdin,
+  output : process.stdout
+})
 let connection;
 
 const setupInput = (conn) => {
@@ -14,19 +20,24 @@ const handleUserInput = function(input) {
     if (input === '\u0003') {
       console.log('Program terminated')
       process.exit();
-    };
+    }
     if (input === 'w') {
       connection.write('Move: up');
-    }
+    } 
     if (input === 'a') {
       connection.write('Move: left');
-    }
+    } 
     if (input === 's') {
       connection.write('Move: down');
-    }
+    } 
     if (input === 'd') {
       connection.write('Move: right');
+    } 
+    if (input === '=') {
+      connection.write(`Say: Whats up`)
+    
     }
+    
 };
 
 module.exports = setupInput
